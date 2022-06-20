7986214c21badbdaecf912d73ccd2e416d781c37
Danilo Petrovic <daniloljubica14@gmail.com>
2022-06-20 23:19:20 +0200

    AnimalList component, animals state, prikaz svih informacija o zivotinjama

---
+++ src/components/AnimalList.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+function AnimalList() {
+
+    const [animals, setAnimals] = useState([
+        { speaces: 'cat', name: 'Marla', age: new Date().toDateString() },
+        { speaces: 'dog', name: 'Rex', age: new Date().toDateString() },
+        { speaces: 'horse', name: 'Black', age: new Date().toDateString() },
+        { speaces: 'bear', name: 'Brundo', age: '' },
+        { speaces: 'fish', name: 'Jonny', age: '' }
+    ])
+
+  return (
+    <div>
+        <ul>
+            { animals.map((animal, index) => (
+                <li key={ index }>
+                    <h3>Animal info: </h3>
+                    <p><b>Speaces: </b>{ animal.speaces }</p>
+                    <p><b>Name: </b>{ animal.name }</p>
+                    <p><b>Date of birth: </b>{ animal.age }</p>
+                </li>
+            )) }
+        </ul>
+    </div>
+  )
+}
+
+export default AnimalList;
