c35cf7f4c29036185c075d8a2cafe8ccc8d197e2
Danilo Petrovic <daniloljubica14@gmail.com>
2022-06-21 14:01:14 +0200

    Zadatak 6 i 7, sektori i prikaz u alertu liste zivotinja koje pripadaju tom sektrou

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react'
 function AnimalList() {
 
     const [animals, setAnimals] = useState([
-        { speaces: 'cat', name: 'Marla', age: new Date().toDateString() },
-        { speaces: 'dog', name: 'Rex', age: new Date().toDateString() },
-        { speaces: 'horse', name: 'Black', age: new Date().toDateString() },
-        { speaces: 'bear', name: 'Brundo', age: '' },
-        { speaces: 'fish', name: 'Jonny', age: '' }
+        { speaces: 'cat', name: 'Marla', age: new Date().toDateString(), sector: 'cats' },
+        { speaces: 'dog', name: 'Rex', age: new Date().toDateString(), sector: 'dogs' },
+        { speaces: 'horse', name: 'Black', age: new Date().toDateString(), sector: 'horses' },
+        { speaces: 'bear', name: 'Brundo', age: '', sector: 'wild animals' },
+        { speaces: 'fish', name: 'Jonny', age: '', sector: 'water animals' }
     ])
+
+    const sectors = ['cats', 'birds', 'dogs', 'horses', 'wild animals', 'snakes', 'water animals']
 
     const handleClick = (e) => {
         const animalToRemove = Number(e.target.parentNode.id);
@@ -34,10 +36,24 @@ function AnimalList() {
         const speacesInput = e.target.speaces.value;
         const nameInput = e.target.name.value;
         const ageInput = e.target.age.value;
+        const sectorInput = e.target.sectors.value;
 
-        const newAnimals = [...animals, {speaces: speacesInput, name: nameInput, age: ageInput}]
+        const newAnimals = [...animals, {speaces: speacesInput, name: nameInput, age: ageInput, sector: sectorInput}]
 
         setAnimals(newAnimals);
+    }
+
+    const checkAnimals = (e) => {
+        const sectorName = e.target.parentNode.firstChild.innerHTML.toString();
+
+        const animalList = [];
+        animals.filter(animal => {
+            if(animal.sector === sectorName) {
+                animalList.push(animal);
+            }
+        })
+
+        alert(animalList.map(animal => 'Animal sector: ' + animal.sector + ', Name: ' + animal.name + ', Speaces: ' + animal.speaces + ', Age: ' + animal.age + '---------------------------'))
     }
 
   return (
@@ -47,6 +63,11 @@ function AnimalList() {
         <input type="text" placeholder='speaces' name='speaces' id='speaces' onChange={e => e.target.value} /><br/>
         <input type="text" placeholder='name' name='name' id='name' onChange={e => e.target.value} /><br/>
         <input type="date" placeholder='date of birth' id='age' onChange={e => e.target.value} /><br/>
+        <label htmlFor="sectors">Sector: </label><select name="sectors" id="sectors" onChange={ e => e.target.value }>
+            { sectors.map((sector, index) => (
+                <option key={index} value={ sector }>{ sector }</option>
+            )) }
+        </select><br/>
         <button>Add Animal</button>
     </form>
 
@@ -57,11 +78,24 @@ function AnimalList() {
                     <p><b>Speaces: </b>{ animal.speaces }</p>
                     <p><b>Name: </b>{ animal.name }</p>
                     <p><b>Date of birth: </b>{ animal.age !== '' ? animal.age : 'Unknown' }</p>
+                    <p><b>Sector: </b>{ animal.sector }</p>
                     <button onClick={handleClick}>Remove</button>
                     <button onClick={moveToTop}>Move to top</button>
                 </li>
             )) }
         </ul>
+
+        <div>
+            <h3>Sectors: </h3>
+            <ul>
+                { sectors.map((sector,index) => (
+                    <li key={index} id={index}>
+                        <p>{ sector }</p>
+                        <button onClick={checkAnimals}>Check animals</button>
+                    </li>
+                )) }
+            </ul>
+        </div>
     </div>
   )
 }
